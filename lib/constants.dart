3c277e2cb44a30a2dc543acab8656bfd29843479
Fady <fedo.zaher@gmail.com>
2024-09-29 08:46:16 +0300

    enhance

--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-class Constants {
-  static const String openAiApiKey =
-      "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";
-}
